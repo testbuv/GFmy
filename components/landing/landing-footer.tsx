3c6c72ac71a58f20ee9f4a6b17ac2330697bce7d
testbuv <testbuv3@gmail.com>
2024-10-08 18:15:11 +0400

    changed footer

--- components/landing/landing-footer.tsx
+++ components/landing/landing-footer.tsx
@@ -1,11 +1,5 @@
 import React from "react";
-import Link from "next/link";
 import Image from "next/image";
-import {
-  GitHubLogoIcon,
-  LinkedInLogoIcon,
-  TwitterLogoIcon,
-} from '@radix-ui/react-icons';
 
 export const LandingFooter = () => {
   return (
@@ -23,62 +17,86 @@ export const LandingFooter = () => {
             />
             <h1 className="ml-2 text-3xl font-bold">GROWTHFAST</h1>
           </div>
+
           <p className="text-lg font-bold">
             AI image generation tool where <br />
             <span className="italic">speed</span> meets{" "}
-            <span className="italic font-normal">precision</span> and <br/>
-            <span className="bg-gradient-to-r from-[#981C82] to-[#FFB017] bg-clip-text text-transparent">imagination</span> knows no limits.
+            <span className="italic font-normal">precision</span> and <br />
+            <span className="bg-gradient-to-r from-[#981C82] to-[#FFB017] bg-clip-text text-transparent">
+              imagination
+            </span>{" "}
+            knows no limits.
           </p>
-          <section className="flex justify-start items-center space-x-6 mt-4">
-            <Image
-              alt="Mastercard"
-              className="aspect-[2/1] overflow-hidden rounded-lg object-contain object-center"
-              height="78"
-              src="/ma_symbol_opt_73_3x.png"
-              width="110"
-            />
-            <Image
-              alt="Visa"
-              className="aspect-[2/1] overflow-hidden rounded-lg object-contain object-center"
-              height="99"
-              src="/visa-secure_blk.png"
-              width="99"
-            />
-          </section>
+
+          <div className="wd-image text-left mt-4">
+            <p className="p1">
+              GrowthFast LTD (15630467)<br />
+              41 Devonshire Street London, W1G 7AJ, United Kingdom
+            </p>
+          </div>
         </div>
+
         <div className="grid grid-cols-2 gap-10 md:col-span-2">
-        <div className="flex flex-col">
-  <h2 className="text-xl font-semibold mb-4">About</h2>
-  <nav className="space-y-2">
-    <Link href="https://growthfast.io/#pricing" className="block hover:text-plum-600 transition-colors duration-200">
-      Pricing
-    </Link>
-    <Link href="https://growthfast.io/#faq" className="block hover:text-plum-600 transition-colors duration-200">
-      FAQ
-    </Link>
-    <Link href="/contact" className="block hover:text-plum-600 transition-colors duration-200">
-      Contact
-    </Link>
-  </nav>
-</div>
+          <div className="flex flex-col">
+            <h2 className="text-xl font-semibold mb-4">About</h2>
+            <nav className="space-y-2">
+              <a
+                href="https://growthfast.io/#pricing"
+                className="block hover:text-plum-600 transition-colors duration-200"
+              >
+                Pricing
+              </a>
+              <a
+                href="https://growthfast.io/#faq"
+                className="block hover:text-plum-600 transition-colors duration-200"
+              >
+                FAQ
+              </a>
+              <a
+                href="/contact"
+                className="block hover:text-plum-600 transition-colors duration-200"
+              >
+                Contact
+              </a>
+            </nav>
+          </div>
+
           <div className="flex flex-col">
             <h2 className="text-xl font-semibold mb-4">Legals</h2>
             <nav className="space-y-2">
-              <Link href="/tc" className="blockhover:text-plum-600 transition-colors duration-200">
+              <a
+                href="/tc"
+                className="block hover:text-plum-600 transition-colors duration-200"
+              >
                 Terms and Conditions
-              </Link>
-              <Link href="/pp" className="block hover:text-plum-600 transition-colors duration-200">
+              </a>
+              <a
+                href="/pp"
+                className="block hover:text-plum-600 transition-colors duration-200"
+              >
                 Privacy Policy
-              </Link>
-              <Link href="/cc" className="block hover:text-plum-600 transition-colors duration-200">
+              </a>
+              <a
+                href="/cc"
+                className="block hover:text-plum-600 transition-colors duration-200"
+              >
                 Cookies Policy
-              </Link>
+              </a>
+              <a
+                href="/cc"
+                className="block hover:text-plum-600 transition-colors duration-200"
+              >
+                Refund Policy
+              </a>
             </nav>
           </div>
         </div>
       </div>
+
       <div className="container mx-auto mt-10 border-t border-gray-700 pt-4 flex flex-col md:flex-row justify-between items-center">
         <p className="text-sm">&copy;2024 GrowthFast All Rights Reserved</p>
+
+        {/* Social */}
         <div className="flex space-x-4 mt-4 md:mt-0">
           <a href="#" aria-label="Facebook">
             <FacebookIcon className="h-6 w-6 text-white" />
@@ -92,12 +110,11 @@ export const LandingFooter = () => {
         </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-interface FacebookIconProps extends React.SVGProps<SVGSVGElement> {}
-
-function FacebookIcon(props: FacebookIconProps) {
+// Facebook
+function FacebookIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
       {...props}
@@ -113,12 +130,11 @@ function FacebookIcon(props: FacebookIconProps) {
     >
       <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
     </svg>
-  )
+  );
 }
 
-interface InstagramIconProps extends React.SVGProps<SVGSVGElement> {}
-
-function InstagramIcon(props: InstagramIconProps) {
+// Instagram
+function InstagramIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
       {...props}
@@ -136,12 +152,11 @@ function InstagramIcon(props: InstagramIconProps) {
       <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
       <line x1="17.5" x2="17.51" y1="6.5" y2="6.5" />
     </svg>
-  )
+  );
 }
 
-interface TwitterIconProps extends React.SVGProps<SVGSVGElement> {}
-
-function TwitterIcon(props: TwitterIconProps) {
+// Twitter
+function TwitterIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
       {...props}
@@ -157,5 +172,5 @@ function TwitterIcon(props: TwitterIconProps) {
     >
       <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z" />
     </svg>
-  )
+  );
 }
